ab474791ed2ef7b15bcfe5f893d03b34268ec412
naveendanu2000 <naveendanu2000@gmail.com>
2026-01-03 23:38:26 +0530

    Deploy: added the depl file for deployement in github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+const isGitHubPages = process.env.GITHUB_PAGES === 'true'
+const repoName = process.env.GITHUB_REPOSITORY ? process.env.GITHUB_REPOSITORY.split('/')[1] : ''
+const base = isGitHubPages && repoName ? `/${repoName}/` : '/'
+
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base,
+  plugins: [react(), tailwindcss()],
 })
