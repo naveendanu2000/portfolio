2df9512be1eddc4db34040c6655241e46f3fc0d6
naveendanu2000 <naveendanu2000@gmail.com>
2026-01-05 18:54:36 +0530

    fixed the footer, a bit smaller and added description

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,3 @@
-import SkillLogo from "./micro-components/SkillLogo";
 import gmail from "/assets/logos/gmail.svg";
 import linkedin from "/assets/logos/linkedin.svg";
 import github from "/assets/logos/github.svg";
@@ -21,7 +20,7 @@ const Footer = () => {
       scrollTrigger: {
         trigger: footerRef.current,
         scrub: true,
-        start: "top 100%",
+        start: "top 105%",
         end: "+=5%",
       },
     });
@@ -31,36 +30,53 @@ const Footer = () => {
     <div
       id="footer"
       ref={footerRef}
-      className="relative overflow-hidden w-full h-[20%] py-[2%] px-[10%] mt-[5%] flex items-center justify-center"
+      className="relative overflow-hidden w-full h-[20%] py-[2%] px-[10%] mt-[5%] flex flex-col items-center justify-center"
     >
       {" "}
-      <div className="z-10 w-full">
+      <div className="z-10 w-full flex flex-wrap items-center justify-center">
         <a href="">
           <div className="inline-flex items-center mx-5 ">
-            <span className="md:text-3xl">Email </span>
-            <SkillLogo imgSrc={gmail} />
+            <img
+              src={gmail}
+              alt="skill logo"
+              className="z-10 relative h-10 w-10"
+            />
+            <span className="md:text-3xl ms-2">Email </span>
           </div>
         </a>
         <a href="https://www.linkedin.com/in/naveen-danu-a4b224293/">
           <div className="inline-flex items-center mx-5 ">
-            <span className="md:text-3xl">Linkedin </span>
-            <SkillLogo imgSrc={linkedin} />
+            <img
+              src={linkedin}
+              alt="skill logo"
+              className="z-10 relative h-10 w-10"
+            />
+            <span className="md:text-3xl ms-2">Linkedin </span>
           </div>
         </a>
         <a href="https://github.com/naveendanu2000">
           <div className="inline-flex items-center mx-5 ">
-            <span className="md:text-3xl">Github </span>
-            <SkillLogo imgSrc={github} />
+            <img
+              src={github}
+              alt="skill logo"
+              className="z-10 relative h-10 w-10"
+            />
+            <span className="md:text-3xl ms-2">Github </span>
           </div>
         </a>
         <a href="https://leetcode.com/u/naveendanu/">
           <div className="inline-flex items-center mx-5 ">
-            <span className="md:text-3xl">Leetcode </span>
-            <SkillLogo imgSrc={leetcode} />
+            <img
+              src={leetcode}
+              alt="skill logo"
+              className="z-10 relative h-10 w-10"
+            />
+            <span className="md:text-3xl ms-2">Leetcode </span>
           </div>
         </a>
       </div>
-      <div className=" z-1 absolute h-full translate-y-25 md:w-[130%] w-[300%] bg-[radial-gradient(ellipse_farthest-side_at_50%_110%,rgba(255,255,255,0.25),rgba(255,255,255,0.2)_30%,rgba(255,255,255,0.2)_80%,rgba(255,255,255,0.09)_92%,transparent)]" />
+      <div className="pt-5">Desgined and built by Naveen Danu</div>
+      <div className=" z-1 absolute h-full translate-y-15 md:w-[130%] w-[300%] bg-[radial-gradient(ellipse_farthest-side_at_50%_110%,rgba(255,255,255,0.25),rgba(255,255,255,0.2)_30%,rgba(255,255,255,0.2)_80%,rgba(255,255,255,0.09)_92%,transparent)]" />
     </div>
   );
 };
