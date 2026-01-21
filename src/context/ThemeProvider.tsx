708eb3e088bba494eca1042f698a9bcbf81cf9e3
Naveen Danu <naveendanu2000@gmail.com>
2026-01-22 02:06:12 +0530

    Added the support for system theme in the website

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,8 +1,22 @@
-import { useState, type ReactNode } from "react";
+import { useState, useEffect, type ReactNode } from "react";
 import { ThemeContext } from "./Theme";
 
 export const ThemeProvider = ({ children }: { children: ReactNode }) => {
-  const [dark, setDark] = useState(true);
+  const [dark, setDark] = useState<boolean>(() => {
+    if (typeof window === "undefined") return false;
+    return window.matchMedia("(prefers-color-scheme: dark)").matches;
+  });
+
+  useEffect(() => {
+    const media = window.matchMedia("(prefers-color-scheme: dark)");
+
+    const listener = (e: MediaQueryListEvent) => {
+      setDark(e.matches);
+    };
+
+    media.addEventListener("change", listener);
+    return () => media.removeEventListener("change", listener);
+  }, []);
 
   const toggleTheme = () => {
     console.log(dark);
